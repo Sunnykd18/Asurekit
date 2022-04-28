edecddc1c5baa409a61f73b1de142323e8d5afb6
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-04-28 21:17:09 +0530

    Assignment

--- flask_auth_app/assignment/main.py
+++ flask_auth_app/assignment/main.py
@@ -1,17 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 from . import db
+from flask_login import login_required, current_user
+
 
 main = Blueprint('main', __name__)
-
 
 @main.route('/')
 def index():
     return render_template('index.html')
 
 @main.route('/profile')
+@login_required
 def profile():
-    return render_template('profile.html')
+    # print(current_user)
+    # print(current_user.username)
+    # print(session)
+    # print(session["_user_id"])
+
+    return render_template('profile.html', name=current_user.username)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
--- flask_auth_app/assignment/models.py
+++ flask_auth_app/assignment/models.py
@@ -1,6 +1,7 @@
 from . import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200))
     email = db.Column(db.String(100), unique=True)
